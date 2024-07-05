e156c4fdbe649d85a9e35ce39293f9bdba7ccbe9
Ramona Banderia <bandeiraramona2@gmail.com>
2024-07-05 17:23:51 -0300

    adicionando versão mobile

--- be-project/src/App.tsx
+++ be-project/src/App.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.css';
 import UserTable from './components/UserTable';
-import {Header} from '../src/components/Header'
+import { Header } from '../src/components/Header';
+import MobileUserTable from './components/MobileUserTable';
+import useWindowDimensions from './hooks/useWindowDimensions';
+import { User } from './types/User';
 
-const App = () => {
+const App: React.FC = () => {
+  const { width } = useWindowDimensions();
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/employees');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError('Erro ao buscar dados.');
+        console.error('Erro ao buscar dados:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div className={styles.container}>
-     <Header/>
-     <h4 className={styles.title}>Funcionários</h4>
-     <UserTable/>
+    <div className="App">
+      <Header />
+      <h4 className={styles.title}>Funcionários</h4>
+      {width > 375 ? (
+        <UserTable users={users} searchTerm={searchTerm} handleSearch={handleSearch} />
+      ) : (
+        <MobileUserTable users={users} searchTerm={searchTerm} handleSearch={handleSearch} />
+      )}
     </div>
   );
 };
--- be-project/src/components/UserTable.tsx
+++ be-project/src/components/UserTable.tsx
@@ -1,53 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { User } from '../types/User';
 import { formatDate } from '../utils/formatDate';
 import { formatPhoneNumber } from '../utils/formatPhone';
 import SearchInput from './SearchInput';
 import styles from '../styles/UserTable.module.css';
 
-const UserTable: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+interface UserTableProps {
+  users: User[];
+  searchTerm: string;
+  handleSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/employees');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        setError('Erro ao buscar dados.');
-        console.error('Erro ao buscar dados:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
-
+const UserTable: React.FC<UserTableProps> = ({ users, searchTerm, handleSearch }) => {
   const filteredUsers = users.filter(user => 
     user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
     user.job.toLowerCase().includes(searchTerm.toLowerCase()) ||
     user.phone.includes(searchTerm)
   );
-
-  if (loading) {
-    return <p>Carregando...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
 
   return (
     <div className={styles.table}>
@@ -55,17 +24,17 @@ const UserTable: React.FC = () => {
       <table>
         <thead>
           <tr>
-            <th className={styles.foto} >FOTO</th>
+            <th className={styles.foto}>FOTO</th>
             <th className={styles.nome}>NOME</th>
             <th className={styles.cargo}>CARGO</th>
-            <th className={styles.data}>DATA DE ADIMISSÃO</th>
+            <th className={styles.data}>DATA DE ADMISSÃO</th>
             <th className={styles.telefone}>TELEFONE</th>
           </tr>
         </thead>
         <tbody>
           {filteredUsers.map(user => (
             <tr key={user.id}>
-              <td><img src={user.image} alt={user.name} /></td>
+              <td><img src={user.image} alt={user.name} className={styles.employeesimg} /></td>
               <td>{user.name}</td>
               <td>{user.job}</td>
               <td>{formatDate(user.admission_date)}</td>
