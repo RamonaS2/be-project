b743470b596b303b6580d6863e05821d9d9d4d1c
Ramona Banderia <bandeiraramona2@gmail.com>
2024-07-06 03:59:02 -0300

    corrigindo useWindowDimensions

--- be-project/src/hooks/useWindowDimensions.ts
+++ be-project/src/hooks/useWindowDimensions.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
+  const { documentElement: {clientHeight: height, clientWidth: width} } = document;
   return {
     width,
     height,
